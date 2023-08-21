fb2768666f02c27821aee5b864ce4238a4c1b5a0
Rasmus Krogsgaard <rasmus.krogsgaard@gmail.com>
2023-08-21 12:52:34 +0200

    diceroller

--- terninge-spil/src/App.jsx
+++ terninge-spil/src/App.jsx
@@ -1,18 +1,40 @@
-import { useState } from 'react'
+import { useState } from "react";
+import "./App.css";
+import img1 from '../src/assets/Alea_1.png'
+import img2 from '../src/assets/Alea_2.png'
+import img3 from '../src/assets/Alea_3.png'
+import img4 from '../src/assets/Alea_4.png'
+import img5 from '../src/assets/Alea_5.png'
+import img6 from '../src/assets/Alea_6.png'
 
 function App() {
+  const Images = [
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6
+   
+  ];
 
-  
+  const [currentFace, setCurrentFace] = useState(Images[0]);
+
+  const rollDice = () => {
+    const randomIndex = Math.floor(Math.random() * Images.length);
+    setCurrentFace(Images[randomIndex]);
+  };
 
   return (
     <>
-      <Player />
-        <div>
-
+      <div>
+        <div className="dice">
+          <img src={currentFace}></img>
+          <button onClick={rollDice}>Slå</button>
         </div>
-      <Player />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
