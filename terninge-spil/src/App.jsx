d0fd59f2b05d765c1e8fdb80b70a45cd7d7f9123
simo994q <simonkn1@hotmail.com>
2023-08-21 11:25:04 +0200

    start

--- terninge-spil/src/App.jsx
+++ terninge-spil/src/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
+import style from './App.scss'
 
 function App() {
 
-  
+
 
   return (
-    <>
+    <div className={style.appContainer}>
       <Player />
         <div>
 
         </div>
       <Player />
-    </>
+    </div>
   )
 }
 
--- terninge-spil/src/components/Players/Players.jsx
+++ terninge-spil/src/components/Players/Players.jsx
@@ -1,3 +1,14 @@
 import style from './Players.module.scss'
 
- 
+export function Players(props) {
+    return (
+        <div className={style.playerContainer}>
+            <h2 className={style.playerNumber}>Spiller {props.player.number}</h2>
+            <div className={style.playerDiceRoll}>
+                <h3>Du slog:</h3>
+                <p></p>
+            </div>
+            <h3 className={style.playerPoints}>Point: {props.player.points}</h3>
+        </div>
+    )
+}
