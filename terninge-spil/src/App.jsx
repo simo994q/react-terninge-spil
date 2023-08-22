d8c75b4b231a03dfb5db9647e9e1ad166fd7a999
simo994q <simonkn1@hotmail.com>
2023-08-22 11:01:13 +0200

    somewhat functional

--- terninge-spil/src/App.jsx
+++ terninge-spil/src/App.jsx
@@ -10,31 +10,67 @@ import img6 from '../src/assets/Alea_6.png'
 
 function App() {
   const Images = [
-    img1,
-    img2,
-    img3,
-    img4,
-    img5,
-    img6
-
+    { image: img1, number: 1 },
+    { image: img2, number: 2 },
+    { image: img3, number: 3 },
+    { image: img4, number: 4 },
+    { image: img5, number: 5 },
+    { image: img6, number: 6 }
   ];
 
-  const [currentFace, setCurrentFace] = useState(Images[0]);
+  const [currentFace, setCurrentFace] = useState(Images[0].image);
+  const [currentRoll, setCurrentRoll] = useState()
+
+  const [playerTurn, setPlayerTurn] = useState(1)
+
+  const [playerOneRoll, setPlayerOneRoll] = useState()
+  const [playerTwoRoll, setPlayerTwoRoll] = useState()
+
+  const [playerOnePoints, setPlayerOnePoints] = useState(0)
+  const [playerTwoPoints, setPlayerTwoPoints] = useState(0)
 
   const rollDice = () => {
     const randomIndex = Math.floor(Math.random() * Images.length);
-    setCurrentFace(Images[randomIndex]);
+    setCurrentFace(Images[randomIndex].image);
+    setCurrentRoll(Images[randomIndex].number)
+
+    if (playerTurn === 1) {
+      setPlayerOneRoll(currentRoll)
+      setPlayerTurn(2)
+      console.log(playerOneRoll);
+    } else if (playerTurn === 2) {
+      setPlayerTwoRoll(currentRoll)
+      if (playerOneRoll === playerTwoRoll) {
+        tieGame()
+      } else {
+        calculateWin()
+      }
+      setPlayerTurn(1)
+      console.log(playerTwoRoll);
+    }
   };
+
+  function tieGame() {
+    alert('Tie Game!')
+  }
+
+  function calculateWin() {
+    if (playerOneRoll > playerTwoRoll) {
+      setPlayerOnePoints(playerOnePoints + 1)
+    } else {
+      setPlayerTwoPoints(playerTwoPoints + 1)
+    }
+  }
 
   return (
     <>
       <div className='appContainer'>
-        <Players />
+        <Players playerPoints={playerOnePoints} playerRoll={playerOneRoll} player={1}/>
         <div className="dice">
           <img src={currentFace}></img>
           <button onClick={rollDice}>Slå</button>
         </div>
-        <Players />
+        <Players playerPoints={playerTwoPoints} playerRoll={playerTwoRoll} player={2}/>
       </div>
     </>
   );
--- terninge-spil/src/components/Players/Players.jsx
+++ terninge-spil/src/components/Players/Players.jsx
@@ -3,16 +3,12 @@ import style from './Players.module.scss'
 export function Players(props) {
     return (
         <div className={style.playerContainer}>
-            <h2 className={style.playerNumber}>Spiller</h2>
+            <h2 className={style.playerNumber}>Spiller {props.player}</h2>
             <div className={style.playerDiceRoll}>
                 <h3>Du slog:</h3>
-                <p></p>
+                <p>{props.playerRoll}</p>
             </div>
-            <h3 className={style.playerPoints}>Point: </h3>
+            <h3 className={style.playerPoints}>Point: {props.playerPoints}</h3>
         </div>
     )
 }
-
-// {props.player.number}
-// {props.player.recentDiceRoll}
-// {props.player.points}
